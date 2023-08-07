e2117cc910a777b69f05fbe5e4ac8a2a5c092208
Dennis Boguslavskiy <db627@njit.edu>
2023-08-07 18:32:08 -0400

    update trading strategy to use Moving Crossover

--- public/analyze.py
+++ public/analyze.py
@@ -1,36 +1,24 @@
-from dotenv import load_dotenv, dotenv_values
-from langchain.llms import OpenAI
-import os
 from dotenv import load_dotenv
-from langchain.prompts import PromptTemplate
+import os
+from langchain.llms import OpenAI
 import robin_stocks.robinhood as r
 
-# load enviornment variables using dotenv package
+# Load environment variables using dotenv package
 load_dotenv()
 
-#connect langchain to the openAI api
-llm = OpenAI(openai_api_key = os.getenv("OPENAI_API_KEY"))
-# get stock data using robinstocks
-def getStockData(stock_name):
-    my_stocks = r.stocks.get_stock_historicals(str(stock_name))
-    return my_stocks
+# Connect langchain to the openAI API
+llm = OpenAI(openai_api_key=os.getenv("OPENAI_API_KEY"))
 
-# get stock data from my account. Loop through dictionary and get the keys (symbols) of my stocks
+def getStockData(stock_name):
+    return r.stocks.get_stock_historicals(stock_name)
+
 def stockNames():
     stock_data = r.account.build_holdings(with_dividends=False)
-    stock_names = []
-    for key, value in stock_data.items():
-        stock_names.append(key)
-    return stock_names
+    return list(stock_data.keys())
 
-# using the names of my stocks, get the data for each stock
 def parseNames(stock_names):
-    info = {}
-    for name in stockNames():
-        info[str(name)] = (getStockData(name))
-    return info
+    return {name: getStockData(name) for name in stock_names}
 
-#format the data into a clean sentance sorting everything by stock
 def format_stock_data(stock_data):
     result = []
     for symbol, data in stock_data.items():
@@ -40,37 +28,49 @@ def format_stock_data(stock_data):
             close_price = entry['close_price']
             high_price = entry['high_price']
             low_price = entry['low_price']
-            line = f"Symbol: {symbol}, Date: {date}, Open Price: {open_price}, Close Price: {close_price}, High Price: {high_price}, Low Price: {low_price}"
-            result.append(line)
+            result.append(f"Symbol: {symbol}, Date: {date}, Open Price: {open_price}, Close Price: {close_price}, High Price: {high_price}, Low Price: {low_price}")
     return '\n'.join(result)
 
-#use that data to predict if to buy or sell the stock
+def moving_average(data, n):
+    cumsum = [0]
+    moving_avgs = []
+    for i, x in enumerate(data, 1):
+        cumsum.append(cumsum[i-1] + x)
+        if i >= n:
+            moving_avg = (cumsum[i] - cumsum[i-n]) / n
+            moving_avgs.append(moving_avg)
+        else:
+            moving_avgs.append(None)
+    return moving_avgs
+
 def langchain(cleaned_stock_data):
     actions = {}
     for symbol, data_entries in cleaned_stock_data.items():
-        data_string = f"Symbol: {symbol}"
-        for entry in data_entries:
-            date = entry['begins_at'].split('T')[0]
-            open_price = entry['open_price']
-            close_price = entry['close_price']
-            high_price = entry['high_price']
-            low_price = entry['low_price']
-            data_string += f", Date: {date}, Open Price: {open_price}, Close Price: {close_price}, High Price: {high_price}, Low Price: {low_price}"
-        if float(data_entries[-1]['close_price']) > float(data_entries[0]['open_price']):
-            action = "buy"
-        elif float(data_entries[-1]['close_price']) < float(data_entries[0]['open_price']):
-            action = "sell"
-        else:
-            action = "hold"
+        closing_prices = [float(entry['close_price']) for entry in data_entries]
 
-        actions[symbol] = action
-    
+        # Use last 5 days of data
+        closing_prices = closing_prices[-5:]
+
+        if len(closing_prices) < 3:
+            actions[symbol] = "hold"
+            continue
+
+        short_term_ma = moving_average(closing_prices, 2)
+        long_term_ma = moving_average(closing_prices, 4)
+
+        if short_term_ma[-1] is not None and long_term_ma[-1] is not None:
+            if short_term_ma[-2] <= long_term_ma[-2] and short_term_ma[-1] > long_term_ma[-1]:
+                actions[symbol] = "buy"
+            elif short_term_ma[-2] >= long_term_ma[-2] and short_term_ma[-1] < long_term_ma[-1]:
+                actions[symbol] = "sell"
+            else:
+                actions[symbol] = "hold"
+        else:
+            actions[symbol] = "hold"
+
     return actions
 
-# organize the predictions from a dictionary to strings
 def organize_langchain(langchain_data):
-    result = []
-    for symbol, action in langchain_data.items():
-        line = f"Symbol: {symbol}, Action: {action}"
-        result.append(line)
-    return '\n'.join(result)
+    return '\n'.join([f"Symbol: {symbol}, Action: {action}" for symbol, action in langchain_data.items()])
+
+
